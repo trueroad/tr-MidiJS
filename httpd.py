382a2c48de1f7ae7f579ea9807799b056984266e
Masamichi Hosoda <trueroad@trueroad.jp>
2023-05-04 14:46:49 +0900

    Add httpd.py

---
+++ httpd.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Httpd for tr-MidiJS sample-BLE-MIDI-to-SMF-cont.
+
+https://github.com/trueroad/tr-MidiJS
+
+Copyright (C) 2023 Masamichi Hosoda.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+"""
+
+from datetime import datetime, timezone
+from pathlib import Path
+from typing import Dict, Final, BinaryIO
+
+from flask import Flask, Response, make_response, request
+
+
+POSTDIR: Final[Path] = Path('post')
+POSTFILE: Final[Path] = Path('post.bin')
+app: Flask = Flask(__name__, static_folder='.', static_url_path='/')
+
+
+def main() -> None:
+    """Test main."""
+    POSTDIR.mkdir(parents=True, exist_ok=True)
+    app.run(debug=True)
+
+
+@app.route('/midi/evaluate', methods=['POST'])
+def evaluate() -> Response:
+    """Evaluate SMF."""
+    dt: datetime = datetime.now(timezone.utc).astimezone()
+
+    fpost: BinaryIO
+    with open(POSTDIR / POSTFILE, 'wb') as fpost:
+        fpost.write(request.get_data())
+
+    dtstr: str = dt.isoformat(timespec='milliseconds'
+                              ).replace('-', '', 2).replace(':', '')
+    request.files['foreval'].save(POSTDIR / Path(f'foreval-{dtstr}.mid'))
+
+    resp: Response = make_response({'Date': dtstr})
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
+    return resp
+
+
+if __name__ == '__main__':
+    main()
