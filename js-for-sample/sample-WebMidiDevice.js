7786adfbdead875c89ac2114ad79b8098a85e3c4
Masamichi Hosoda <trueroad@trueroad.jp>
2023-05-05 20:05:24 +0900

    Add WebMidiDevice.js etc.

---
+++ js-for-sample/sample-WebMidiDevice.js
@@ -0,0 +1,151 @@
+/**
+ * Sample using WebMidiDevice.
+ * @module sample-WebMidiDevice
+ * @author Masamichi Hosoda <trueroad@trueroad.jp>
+ * @copyright (C) Masamichi Hosoda 2023
+ * @license BSD-2-Clause
+ * @see {@link https://github.com/trueroad/tr-MidiJS}
+ */
+
+import {WebMidiDevice} from "../js/WebMidiDevice.js";
+
+/**
+ * WebMidiDevice class instance.
+ * @type {module:WebMidiDevice.WebMidiDevice}
+ */
+const webMidiDevice = new WebMidiDevice();
+
+//
+// Element ID
+//
+
+// Button
+const getMidiPortButton = document.getElementById("getMidiPortButton");
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+const sendButton = document.getElementById("sendButton");
+
+const clearMidiMessageReceivedLogButton =
+      document.getElementById("clearMidiMessageReceivedLogButton");
+
+// Status span
+const getMidiPortStatus = document.getElementById("getMidiPortStatus");
+
+// Select Port
+const selectMidiInPort = document.getElementById("selectMidiInPort");
+const selectMidiOutPort = document.getElementById("selectMidiOutPort");
+
+// Input text
+const sendMessage = document.getElementById("sendMessage");
+
+// Log textarea
+const midiMessageReceivedLog =
+      document.getElementById("midiMessageReceivedLog");
+
+//
+// Logging
+//
+
+/**
+ * Sample handler function to be called when a MIDI message is received.
+ * @param {number} delta - Delta time in millisecond.
+ * @param {Uint8Array} message - Received MIDI message.
+ */
+function sampleHandlerMidiMessageReceived(delta, message) {
+  // Call default handler
+  webMidiDevice.defaultHandlerMidiMessageReceived(delta, message);
+
+  // Logging
+  midiMessageReceivedLog.value +=
+    JSON.stringify({"delta": delta,
+                    "message": Array.from(message)}) + "\n";
+  // Scrolling
+  midiMessageReceivedLog.scrollTop = midiMessageReceivedLog.scrollHeight;
+}
+
+// Set the sample handler function.
+webMidiDevice.handlerMidiMessageReceived = sampleHandlerMidiMessageReceived;
+
+//
+// UI function
+//
+
+async function getMidiPort() {
+  getMidiPortStatus.innerText = "pending...";
+
+  try {
+    await webMidiDevice.buildMidiPortList();
+  } catch (err) {
+    getMidiPortStatus.innerText = err;
+    return;
+  }
+
+  while (selectMidiInPort.lastChild) {
+    selectMidiInPort.removeChild(selectMidiInPort.lastChild);
+  }
+  while (selectMidiOutPort.lastChild) {
+    selectMidiOutPort.removeChild(selectMidiOutPort.lastChild);
+  }
+
+  for (const id of webMidiDevice.inputIDs) {
+    console.log(id + ": " + webMidiDevice.getMidiInPortName(id));
+
+    const opt = document.createElement("option");
+    opt.text = webMidiDevice.getMidiInPortName(id);
+    opt.value = id;
+    selectMidiInPort.appendChild(opt);
+  }
+  for (const id of webMidiDevice.outputIDs) {
+    console.log(id + ": " + webMidiDevice.getMidiOutPortName(id));
+
+    const opt = document.createElement("option");
+    opt.text = webMidiDevice.getMidiOutPortName(id);
+    opt.value = id;
+    selectMidiOutPort.appendChild(opt);
+  }
+
+  getMidiPortStatus.innerText = "Succeeded";
+}
+
+function start() {
+  console.log("*** Start: " + selectMidiInPort.value + " ***");
+  webMidiDevice.start(selectMidiInPort.value);
+}
+
+function stop() {
+  console.log("*** Stop ***");
+  webMidiDevice.stop();
+}
+
+function send() {
+  const message = new Uint8Array(JSON.parse(sendMessage.value)["message"]);
+  let a = [];
+  for (let i = 0; i < message.byteLength; i++) {
+    a.push("0x" + message[i].toString(16).padStart(2, "0"));
+  }
+  console.log("*** Send message ***")
+  console.log("  " + selectMidiOutPort.value + ": " + a.join(" "));
+
+  webMidiDevice.send(selectMidiOutPort.value, message);
+}
+
+function clearMidiMessageReceivedLog() {
+  midiMessageReceivedLog.value = "";
+}
+
+//
+// Add event listener.
+//
+
+getMidiPortButton &&
+  getMidiPortButton.addEventListener("click", getMidiPort);
+startButton &&
+  startButton.addEventListener("click", start);
+stopButton &&
+  stopButton.addEventListener("click", stop);
+sendButton &&
+  sendButton.addEventListener("click", send);
+
+clearMidiMessageReceivedLogButton &&
+  clearMidiMessageReceivedLogButton.addEventListener(
+    "click", clearMidiMessageReceivedLog);
---
+++ js/WebMidiDevice.js
@@ -0,0 +1,183 @@
+/**
+ * Web MIDI device module.
+ * @module WebMidiDevice
+ * @author Masamichi Hosoda <trueroad@trueroad.jp>
+ * @copyright (C) Masamichi Hosoda 2023
+ * @license BSD-2-Clause
+ * @see {@link https://github.com/trueroad/tr-MidiJS}
+ */
+
+/**
+ * Web MIDI device class.
+ */
+export class WebMidiDevice {
+  /**
+   * Constructor.
+   */
+  constructor() {
+    console.log("WebMidiDevice.constructor()");
+
+    /**
+     * Specify a handler function to be called when a MIDI message is received.
+     *     If null, the default handler is called.
+     * @member {?module:WebMidiDevice.WebMidiDevice~HandlerMidiMessageReceived}
+     */
+    this.handlerMidiMessageReceived = null;
+
+    /**
+     * MIDI IN port IDs.
+     * @type {string[]}
+     */
+    this.inputIDs = [];
+    /**
+     * MIDI OUT port IDs.
+     * @type {string[]}
+     */
+    this.outputIDs = [];
+
+    this._inputList = {};
+    this._outputList = {};
+
+    this._input = null;
+    this._eventTimestampBefore = null;
+
+    this._onMidiMessageBinded = this.onMidiMessage.bind(this);
+  }
+
+  /**
+   * Build MIDI IN/OUT port list.
+   */
+  async buildMidiPortList() {
+    console.log("WebMidiDevice.buildMidiPortList()");
+
+    let access;
+    try {
+      access = await navigator.requestMIDIAccess({sysex: true,
+                                                  software: true});
+    } catch (err) {
+      console.log("WebMidiDevice.buildMidiPortList(): " +
+                  "navigator.requestMIDIAccess() error: " + err);
+      throw err;
+    }
+
+    this._inputList = {};
+    this.inputIDs = [];
+    access.inputs.forEach(input => {
+      this._inputList[input.id] = input;
+      this.inputIDs.push(input.id);
+    });
+
+    this._outputList = {};
+    this.outputIDs = [];
+    access.outputs.forEach(output => {
+      this._outputList[output.id] = output;
+      this.outputIDs.push(output.id);
+    });
+  }
+
+  /**
+   * Get MIDI IN port.
+   * @param {string} id - MIDI IN port ID.
+   * @return {MIDIInput} MIDI IN port.
+   */
+  getMidiInPort(id) {
+    return this._inputList[id];
+  }
+
+  /**
+   * Get MIDI OUT port.
+   * @param {string} id - MIDI OUT port ID.
+   * @return {MIDIOutput} MIDI OUT port.
+   */
+  getMidiOutPort(id) {
+    return this._outputList[id];
+  }
+
+  /**
+   * Get MIDI IN port name.
+   * @param {string} id - MIDI IN port ID.
+   * @return {string} MIDI IN port name.
+   */
+  getMidiInPortName(id) {
+    return this._inputList[id].name;
+  }
+
+  /**
+   * Get MIDI OUT port name.
+   * @param {string} id - MIDI OUT port ID.
+   * @return {string} MIDI OUT port name.
+   */
+  getMidiOutPortName(id) {
+    return this._outputList[id].name;
+  }
+
+  /**
+   * Start receiving MIDI messages.
+   * @param {string} id - MIDI IN port ID.
+   */
+  start(id) {
+    if (this._input) {
+      this.stop();
+    }
+
+    this._input = this.getMidiInPort(id);
+    this._eventTimestampBefore = null;
+    this._input.addEventListener("midimessage", this._onMidiMessageBinded);
+  }
+
+  /**
+   * Stop receiving MIDI messages.
+   */
+  stop() {
+    if (this._input) {
+      this._input.removeEventListener("midimessage",
+                                      this._onMidiMessageBinded);
+      this._input = null;
+    }
+  }
+
+  /**
+   * Send MIDI message.
+   * @param {string} id - MIDI OUT port ID.
+   * @param {Uint8Array} message - MIDI message to send.
+   */
+  send(id, message) {
+    this.getMidiOutPort(id).send(message);
+  }
+
+  /**
+   * Event listener for "onmidimessage".
+   * @param {Event} event - Information of the event.
+   */
+  onMidiMessage(event) {
+    const eventDelta = this._eventTimestampBefore === null
+          ? 0
+          : event.timeStamp - this._eventTimestampBefore;
+    this._eventTimestampBefore = event.timeStamp;
+
+    if (this.handlerMidiMessageReceived) {
+      this.handlerMidiMessageReceived(eventDelta, event.data);
+    } else {
+      this.defaultHandlerMidiMessageReceived(eventDelta, event.data);
+    }
+  }
+
+  /**
+   * Handler function to be called when a MIDI message is received.
+   * @callback module:WebMidiDevice.WebMidiDevice~HandlerMidiMessageReceived
+   * @param {number} delta - Delta time in millisecond.
+   * @param {Uint8Array} message - Received MIDI message.
+   */
+
+  /**
+   * Default handler function to be called when a MIDI message is received.
+   * @param {number} delta - Delta time in millisecond.
+   * @param {Uint8Array} message - Received MIDI message.
+   */
+  defaultHandlerMidiMessageReceived(delta, message) {
+    console.log("*** MIDI message recieved ***");
+    console.log("  received: " +
+                JSON.stringify({"delta": delta,
+                                "message": Array.from(message)}));
+  }
+}
